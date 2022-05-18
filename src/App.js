b89e1ebbc00ca47b1b12a1656dc92a3cf260b5b2
basilasalim27 <85999501+basilasalim27@users.noreply.github.com>
2022-05-18 16:56:01 +0530

    todo-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="todo1">
+      <div class="todo2">
+        <h4 ><b>My Todo App</b></h4>
+        <input type="text" id="myInput" placeholder="Enter a todo"></input>
+        <span onclick="newElement()" class="addBtn" id="btn">✔</span>
+      </div>
     </div>
   );
 }
