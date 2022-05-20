cf9c5337333bd7328bfd528a38a44c9f71b7b663
basilasalim27 <85999501+basilasalim27@users.noreply.github.com>
2022-05-20 17:46:53 +0530

    todo list

--- src/App.js
+++ src/App.js
@@ -4,7 +4,26 @@ function App() {
   return (
     <div class="container">
       <div class="todo">My Todo App</div>
+      <div class="one">
+        <img src="https://img.icons8.com/ios/30/1ABC9C/unchecked-circle.png" /> Buy the groceries
+      </div>
+      <div class="two">
+        <img src="https://img.icons8.com/ios/30/1ABC9C/unchecked-circle.png" /> Get the items
+      </div>
+      <div class="three">
+        <img src="https://img.icons8.com/ios/30/1ABC9C/unchecked-circle.png" /> Push to Github
+      </div>
+      <div class="four"></div>
       <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+
+
       <div class="bottom-bar">
         <input type="text" class="textarea" placeholder="Enter a todo"></input>
         <button>
