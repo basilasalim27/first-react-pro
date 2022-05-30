073f4a6951502efc5f00e690f9018f4013abad89
basilasalim27 <85999501+basilasalim27@users.noreply.github.com>
2022-05-30 15:57:16 +0530

    initial stage completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
+import { useState } from 'react'
+
 function App() {
-  const todoItems = [{ taskId: 1, taskStatus: true, taskDescription: "Buy the groceries" }, { taskId: 2, taskStatus: false, taskDescription: "Push to Github" }]
+  //const todoItems = []
+  const [todoItems, setTodoItems] = useState([])
+  const [taskDescription, setTaskDescription] = useState("")
+
+  //const todoItems = state[0]
+  //const setTodoItems = state[1]
   function handleSubmitButtonClicked() {
-    alert("Hello! I am an alert box!");
+    let heighest = 0
+    for (const i of todoItems) {
+      heighest = i.taskId
+    }
+    const todoItem = {
+      taskId: heighest + 1,
+      taskStatus: true,
+      taskDescription: taskDescription
+    }
+    todoItems.push(todoItem)
+    setTodoItems([...todoItems])
+    setTaskDescription("")
+
   }
   function handleCheckedButtonClicked(taskId) {
-    alert(taskId)
+    todoItems[taskId - 1].taskStatus = !todoItems[taskId - 1].taskStatus
+    //const index = todoItems.findIndex(item=> item.taskId==taskId)
+    setTodoItems([...todoItems])
+  }
+  function handleDescriptionChanged(e) {
+    setTaskDescription(e.target.value)
   }
   return (
     <div className="container">
@@ -19,14 +43,14 @@ function App() {
             </div>)
           }
           return (<div className="list" key={item.taskId}>
-            <button onClick={() => handleCheckedButtonClicked(item.taskId)} class="checked">
+            <button onClick={() => { handleCheckedButtonClicked(item.taskId) }} class="checked">
               <img src="https://img.icons8.com/fluency/34/000000/ok.png" className="image" /> </button> <div className="task-description" key={item.taskId}> {item.taskDescription} </div>
           </div>)
         })
         }
       </div>
       <div className="bottom-bar">
-        <input type="text" className="textarea" placeholder="Enter a todo"></input>
+        <input type="text" value={taskDescription} onChange={handleDescriptionChanged} className="textarea" placeholder="Enter a todo"></input>
         <button onClick={handleSubmitButtonClicked}>
           <img src="https://img.icons8.com/external-royyan-wijaya-detailed-outline-royyan-wijaya/24/ffffff/external-tick-interface-royyan-wijaya-detailed-outline-royyan-wijaya-4.png" />
         </button>
