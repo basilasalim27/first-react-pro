cc4ef2a54aeacf42898b68da1c44baebcee173fc
basilasalim27 <85999501+basilasalim27@users.noreply.github.com>
2022-05-31 17:42:30 +0530

    delete button added

--- src/App.js
+++ src/App.js
@@ -21,16 +21,25 @@ function App() {
     todoItems.push(todoItem)
     setTodoItems([...todoItems])
     setTaskDescription("")
-
   }
+
   function handleCheckedButtonClicked(taskId) {
     todoItems[taskId - 1].taskStatus = !todoItems[taskId - 1].taskStatus
-    //const index = todoItems.findIndex(item=> item.taskId==taskId)
+    //const index = todoItems.findIndex(item => item.taskId == taskId)
     setTodoItems([...todoItems])
   }
+
   function handleDescriptionChanged(e) {
     setTaskDescription(e.target.value)
+    //console.log("enter", e.target.value);
   }
+
+  function handleCloseButton(taskId) {
+    const newTodoItems = todoItems.filter(item => taskId != item.taskId)
+    todoItems.pop()
+    setTodoItems([...todoItems])
+  }
+
   return (
     <div className="container">
       <div className="todo">My Todo App</div>
@@ -38,13 +47,19 @@ function App() {
         {todoItems.map(item => {
           if (item.taskStatus) {
             return (<div className="list" key={item.taskId}>
-              <button onClick={() => handleCheckedButtonClicked(item.taskId)} class="checked">
+              <button onClick={() => handleCheckedButtonClicked(item.taskId)} className="checked">
                 <img src="https://img.icons8.com/ios/30/1ABC9C/unchecked-circle.png" className="image" /> </button> {item.taskDescription}
+              <button onClick={() => handleCloseButton(item.taskId)} className='deleted'>
+                <img src="https://img.icons8.com/material-rounded/24/1ABC9C/filled-trash.png" />
+              </button>
             </div>)
           }
           return (<div className="list" key={item.taskId}>
-            <button onClick={() => { handleCheckedButtonClicked(item.taskId) }} class="checked">
+            <button onClick={() => { handleCheckedButtonClicked(item.taskId) }} className="checked">
               <img src="https://img.icons8.com/fluency/34/000000/ok.png" className="image" /> </button> <div className="task-description" key={item.taskId}> {item.taskDescription} </div>
+            <button onClick={() => handleCloseButton(item.taskId)} className='deleted'>
+              <img src="https://img.icons8.com/material-rounded/24/1ABC9C/filled-trash.png" />
+            </button>
           </div>)
         })
         }
