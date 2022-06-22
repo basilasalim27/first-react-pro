935721151c9855e4f3d4e9e969aa990eedc03b60
basilasalim27 <85999501+basilasalim27@users.noreply.github.com>
2022-06-22 17:37:06 +0530

    signin page

--- src/component/Signin/Signin.js
+++ src/component/Signin/Signin.js
@@ -1,11 +1,22 @@
 import React from "react";
 import './Signin.css';
+import ReactDOM from 'react-dom/client';
 import { Link } from "react-router-dom"
 const Signin = () => {
     return (
-        <header>
-            <Link to="/">Sign in</Link>
-        </header>
+        <form>
+            <label>User Id:
+                <input type="text" value={"admin"} />
+            </label>
+            <br></br>
+            <label>Password:
+                <input type="Password" value={"admin"} />
+            </label>
+            <br></br>
+            <button>
+                <Link to="/">Sign in</Link>
+            </button>
+        </form>
     );
 };
 
